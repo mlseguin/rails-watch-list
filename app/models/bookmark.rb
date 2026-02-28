4feed785916bc733c7b1b8c92d57202422357832
Marie-Laure Seguin <mlseguin@gmail.com>
2026-02-28 11:27:18 +0100

    finishing models configuration

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,13 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  # not shorter than 5 characters
+  validates :comment, length: { minimum: 6 }
+
+  # each movie can only be once in a list
+  validates :movie, uniqueness: {
+    scope: :list_id,
+    message: "Already bookmarked in this list"
+  }
 end
